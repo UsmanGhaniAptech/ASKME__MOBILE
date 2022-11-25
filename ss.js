186b179ce9467a8f0c67318f4d1b296a39169e30
Student <lab-203@aptech-msg.edu.pk>
2022-11-25 12:32:17 +0500

    i have edited ss.js

---
+++ ss.js
@@ -0,0 +1,144 @@
+var card1 =
+[
+ {"Id":0,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":1,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":2,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."}
+ 
+ 
+]
+var card2 =
+[
+ {"Id":0,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":1,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":2,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."}
+ 
+ 
+]
+var card3 =
+[
+ {"Id":0,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":1,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":2,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."}
+ 
+ 
+]
+var card4 =
+[
+ {"Id":0,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":1,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":2,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."}
+ 
+ 
+]
+var card5 =
+[
+ {"Id":0,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":1,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":2,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."}
+ 
+ 
+]
+var card6 =
+[
+ {"Id":0,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":1,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":2,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."}
+ 
+ 
+]
+var card7 =
+[
+ {"Id":0,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":1,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":2,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."}
+ 
+ 
+]
+var card8 =
+[
+ {"Id":0,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":1,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":2,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."}
+ 
+ 
+]
+var card9 =
+[
+ {"Id":0,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":1,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":2,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."}
+ 
+ 
+]
+var card10 =
+[
+ {"Id":0,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":1,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":2,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."}
+ 
+ 
+]
+var card11 =
+[
+ {"Id":0,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":1,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":2,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."}
+ 
+ 
+]
+var card12 =
+[
+ {"Id":0,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":1,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":2,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."}
+ 
+ 
+]
+var card13 =
+[
+ {"Id":0,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":1,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":2,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."}
+ 
+ 
+]
+var card14 =
+[
+ {"Id":0,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":1,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":2,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."}
+ 
+ 
+]
+var card15 =
+[
+ {"Id":0,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":1,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":2,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."}
+ 
+ 
+]
+var card16 =
+[
+ {"Id":0,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":1,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":2,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."}
+ 
+ 
+]
+var card17 =
+[
+ {"Id":0,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":1,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":2,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."}
+ 
+ 
+]
+var card18 =
+[
+ {"Id":0,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":1,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":2,"Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."}
+ 
+ 
+]
